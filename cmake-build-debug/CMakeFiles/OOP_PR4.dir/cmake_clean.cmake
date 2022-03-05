eeb7f41c81cc3b3760948b8a8efffa35b9d21148
Alex Vaulin <vau1in.alexey@yandex.ru>
2022-03-05 13:25:51 +0300

    First Project

---
+++ cmake-build-debug/CMakeFiles/OOP_PR4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_PR4.dir/main.cpp.obj"
+  "OOP_PR4.exe"
+  "OOP_PR4.exe.manifest"
+  "OOP_PR4.pdb"
+  "libOOP_PR4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_PR4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OOP_PR4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OOP_PR4.
+# This may be replaced when dependencies are built.
